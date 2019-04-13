3a8f1da3b9c1140163228354fded324254f4c2f9
Promise Ihunna <PromiseIhunna@gmail.com>
2019-04-13 02:54:33 +0100

    fixed config

--- resources/config.php
+++ resources/config.php
@@ -1,17 +1,19 @@
 <?php
 
-  return (object) array(
-     "host"=> "localhost",
-     "username"=> "root",
-     "password" => "",
-     "db" => "nigeria"
-  );
   // return (object) array(
   //    "host"=> "localhost",
-  //    "username"=> "mukaznig_mukaznig",
-  //    "password" => "saveminaj.badguy123",
-  //    "db" => "mukaznig_nigeria"
+  //    "username"=> "root",
+  //    "password" => "",
+  //    "db" => "nigeria"
   // );
+
+
+  return (object) array(
+     "host"=> "localhost",
+     "username"=> "mukaznig_mukaznig",
+     "password" => "saveminaj.badguy123",
+     "db" => "mukaznig_nigeria"
+  );
 
 
 ?>
